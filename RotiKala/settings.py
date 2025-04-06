ed3a1efe88a00bea85c9eb923cac0c712c79392f
Ashkan <ashkanghodrati0@gmail.com>
2025-04-06 21:46:30 +0330

    create account

--- RotiKala/settings.py
+++ RotiKala/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "core.apps.CoreConfig"
+    "core.apps.CoreConfig",
+    "account.apps.AccountConfig"
 ]
 
 MIDDLEWARE = [
