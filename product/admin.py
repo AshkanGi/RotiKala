32b51ea806d6a7a75c07dfc141f44ff3733b3a61
Ashkan <ashkanghodrati0@gmail.com>
2025-04-07 18:00:26 +0330

    product run

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import Product, Size, Color, ProductGallery, TopMaterial, SoleMaterial, InsoleModel, OpenClose, Comment, Category
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'price', 'discount_percentage', 'discounted_price', 'created_at', 'update_at')
+    exclude = ('slug',)
+    list_filter = ('created_at', 'update_at', 'discount_percentage')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent')
+    list_filter = ('parent',)
+    search_fields = ('title',)
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ('color',)
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ('size',)
+
+
+@admin.register(TopMaterial)
+class TopMaterialAdmin(admin.ModelAdmin):
+    list_display = ('top_material',)
+
+
+@admin.register(SoleMaterial)
+class SoleMaterialAdmin(admin.ModelAdmin):
+    list_display = ('sole_material',)
+
+
+@admin.register(InsoleModel)
+class InsoleModelAdmin(admin.ModelAdmin):
+    list_display = ('insole_model',)
+
+
+@admin.register(OpenClose)
+class OpenCloseAdmin(admin.ModelAdmin):
+    list_display = ('open_close',)
+
+
+@admin.register(ProductGallery)
+class ProductGalleryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'alt_text')
+
+
+admin.site.register(Comment)
--- product/views.py
+++ product/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.contrib import messages
+from .forms import CommentCreatForm
+from .models import Product, Comment
+from cart.cart_module import Cart
+from django.shortcuts import redirect
+from django.views.generic import DetailView
 
-# Create your views here.
+
+class ProductView(DetailView):
+    template_name = 'product/product-detail.html'
+    model = Product
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentCreatForm()
+        context['cart'] = Cart(self.request)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        product = self.get_object()
+        form = CommentCreatForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            comment = Comment()
+            comment.user = request.user
+            comment.product = product
+            comment.title = cd['title']
+            comment.body = cd['body']
+            recommend_value = request.POST.get('recommend')
+            if recommend_value is not None:
+                comment.is_recommended = bool(int(recommend_value))
+            else:
+                comment.is_recommended = False
+            comment.save()
+            return redirect('ProductApp:product_detail', slug=product.slug)
+        messages.error(request, 'اطلاعات وارد شده مناسب  نیست , لطفا مجدد تلاش کنید.')
+        return self.get(request, *args, **kwargs)
